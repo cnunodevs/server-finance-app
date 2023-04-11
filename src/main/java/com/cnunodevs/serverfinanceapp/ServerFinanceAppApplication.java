39448d7e4acc72eac629629da802348fde12cf0b
cristianPorrety <cristianporretydev@gmail.com>
2023-04-11 17:52:52 -0500

    initialized project

---
+++ src/main/java/com/cnunodevs/serverfinanceapp/ServerFinanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.cnunodevs.serverfinanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerFinanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerFinanceAppApplication.class, args);
+	}
+
+}
