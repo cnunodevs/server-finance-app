5e5a699cc90508434e032f94231ed9f4fb53b027
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-04-13 11:36:07 -0500

    PortafolioMapper completed

--- src/main/java/com/cnunodevs/serverfinanceapp/model/mapper/PortafolioMapper.java
+++ src/main/java/com/cnunodevs/serverfinanceapp/model/mapper/PortafolioMapper.java
@@ -3,21 +3,37 @@ package com.cnunodevs.serverfinanceapp.model.mapper;
 import org.springframework.stereotype.Service;
 
 import com.cnunodevs.serverfinanceapp.model.dto.PortafolioDTO;
+import com.cnunodevs.serverfinanceapp.model.entity.Objetivo;
 import com.cnunodevs.serverfinanceapp.model.entity.Portafolio;
+import com.cnunodevs.serverfinanceapp.model.entity.Usuario;
 
 @Service
-public class PortafolioMapper implements GenericMapper<Portafolio, PortafolioDTO>{
+public class PortafolioMapper implements GenericMapper<Portafolio, PortafolioDTO> {
 
     @Override
     public Portafolio dtoToPojo(PortafolioDTO dto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dtoToPojo'");
+        Portafolio portafolio = Portafolio.builder()
+                .nombre(dto.getNombre())
+                .descripcion(dto.getDescripcion())
+                .usuario(Usuario.builder().id(dto.getIdUsuario()).build())
+                .objetivo(Objetivo.builder().id(dto.getIdObjetivo()).build())
+                .build();
+        if (dto.getId() != null) {
+            portafolio.setId(dto.getId());
+        }
+        return portafolio;
     }
 
     @Override
     public PortafolioDTO pojoToDto(Portafolio pojo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'pojoToDto'");
+        PortafolioDTO portafolioDTO = PortafolioDTO.builder()
+                .id(pojo.getId())
+                .nombre(pojo.getNombre())
+                .descripcion(pojo.getDescripcion())
+                .idUsuario(pojo.getUsuario().getId())
+                .idObjetivo(pojo.getObjetivo().getId())
+                .build();
+        return portafolioDTO;
     }
-    
+
 }
