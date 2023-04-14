7ee584ce8278568f49dc731349367f6d0dfe49b9
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-04-14 18:58:43 -0500

    More options for tipoMovimiento

--- src/main/java/com/cnunodevs/serverfinanceapp/model/entity/enums/TipoMovimiento.java
+++ src/main/java/com/cnunodevs/serverfinanceapp/model/entity/enums/TipoMovimiento.java
@@ -2,5 +2,7 @@ package com.cnunodevs.serverfinanceapp.model.entity.enums;
 
 public enum TipoMovimiento {
     INGRESO,
-    EGRESO
+    EGRESO,
+    TRANSFERENCIA_AHORRO,
+    TRANSFERENCIA_DISPONIBLE
 }
