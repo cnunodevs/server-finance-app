3ad6499dda46cea3d93eb897e26cf3d4180d9864
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-04-14 19:05:30 -0500

    MetricaInversion fields logic extracted to local methods

--- src/main/java/com/cnunodevs/serverfinanceapp/model/domain/MetricaInversion.java
+++ src/main/java/com/cnunodevs/serverfinanceapp/model/domain/MetricaInversion.java
@@ -17,13 +17,33 @@ public class MetricaInversion {
 
     public MetricaInversion(Inversion inversion) {
 
-        this.idInversion = inversion.getId();
-        this.valorTotal = inversion.getCantidad() * inversion.getPrecio().doubleValue();
-        this.gananciaEsperada = (inversion.getCantidad() * inversion.getPrecio().doubleValue())
-                * inversion.getRentabilidadEsperada().doubleValue() - inversion.getPrecio().doubleValue();
-        this.plazo = inversion.getPlazo().toString();
-        this.nivelRiesgo = inversion.getPerfilRiesgo().toString();
+        this.idInversion = idInversion(inversion);
+        this.valorTotal = valorTotal(inversion);
+        this.gananciaEsperada = gananciaEsperada(inversion);
+        this.plazo = plazo(inversion);
+        this.nivelRiesgo = nivelRiesgo(inversion);
 
+    }
+
+    private double gananciaEsperada(Inversion inversion) {
+        return valorTotal(inversion)
+                * inversion.getRentabilidadEsperada().doubleValue() - inversion.getPrecio().doubleValue();
+    }
+
+    private String nivelRiesgo(Inversion inversion) {
+        return inversion.getPerfilRiesgo().toString();
+    }
+
+    private String plazo(Inversion inversion) {
+        return inversion.getPlazo().toString();
+    }
+
+    private double valorTotal(Inversion inversion) {
+        return inversion.getCantidad() * inversion.getPrecio().doubleValue();
+    }
+
+    private UUID idInversion(Inversion inversion) {
+        return inversion.getId();
     }
 
 }
