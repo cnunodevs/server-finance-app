c63495688d5c86adf8d84799f3dc655eafe5f3a4
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-04-15 16:37:43 -0500

    Commets added to inversionesController

--- src/main/java/com/cnunodevs/serverfinanceapp/controller/InversionesController.java
+++ src/main/java/com/cnunodevs/serverfinanceapp/controller/InversionesController.java
@@ -44,6 +44,12 @@ public class InversionesController {
     private final InversionesService inversionesService;
     private final InversionMapper inversionMapper;
 
+    /**
+    Obtiene las métricas de una inversión especificada por su ID.
+    @param idInversion El ID de la inversión de la que se desean obtener las métricas.
+    @return Un objeto ResponseEntity con un cuerpo que contiene las métricas de la inversión y un código de estado HTTP que indica el resultado de la operación.
+    @throws EntityNotFoundException Si la inversión especificada no existe.
+    */
     @GetMapping("/metricas/{idInversion}")
     public ResponseEntity<MetricaInversion> handleGetMetricasInversion(@PathVariable final UUID idInversion)
             throws EntityNotFoundException {
@@ -55,6 +61,11 @@ public class InversionesController {
         return ResponseEntity.status(HttpStatus.OK).body(metrica);
     }
 
+    /**
+    Obtiene las métricas de inversión para un portafolio especificado por su ID.
+    @param idPortafolio El ID del portafolio del que se desean obtener las métricas de inversión.
+    @return Un objeto ResponseEntity con un código de estado HTTP que indica el resultado de la operación y la lista de métricas de inversión obtenida.
+    */    
     @GetMapping("/metricas")
     public ResponseEntity<List<MetricaInversion>> handleGetMetricasInversiones(@RequestParam final UUID idPortafolio) {
         final List<Inversion> inversiones = inversionesService.findInversionesByPortafolioId(idPortafolio);
@@ -63,6 +74,13 @@ public class InversionesController {
         return ResponseEntity.status(HttpStatus.OK).body(metricas);
     }
 
+    /**
+    Obtiene una página de inversiones de un portafolio especificado por su ID.
+    @param page El número de página solicitado (por defecto es 0).
+    @param size El tamaño de la página (por defecto es 9).
+    @param idPortafolio El ID del portafolio al que pertenecen las inversiones.
+    @return Un objeto ResponseEntity con una página de objetos InversionDTO que corresponden a las inversiones del portafolio especificado.
+    */
     @GetMapping
     public ResponseEntity<Page<InversionDTO>> handleGetInversiones(
             @RequestParam(defaultValue = "0") final Integer page,
@@ -77,6 +95,12 @@ public class InversionesController {
         return ResponseEntity.status(HttpStatus.OK).body(pageInversionesDTO);
     }
 
+    /**
+    Obtiene una inversión específica a partir de su ID.
+    @param idInversion El ID de la inversión que se desea obtener.
+    @return Un objeto ResponseEntity que contiene un objeto InversionDTO y un código de estado HTTP que indica el resultado de la operación.
+    @throws EntityNotFoundException Si la inversión especificada no existe.
+    */
     @GetMapping("/{idInversion}")
     public ResponseEntity<InversionDTO> handleGetInversionById(@PathVariable final UUID idInversion) {
         if (!inversionesService.inversionAlreadyExist(idInversion)) {
@@ -87,6 +111,12 @@ public class InversionesController {
         return ResponseEntity.status(HttpStatus.OK).body(inversionDTO);
     }
 
+    /**
+    Crea una nueva inversion en el sistema.
+    @param inversionDTO El objeto InversionDTO que contiene los detalles de la nueva inversion a crear.
+    @return Un objeto ResponseEntity con un código de estado HTTP que indica el resultado de la operación.
+    @throws IllegalStateException Si ya existe una inversion con el mismo nombre en el portafolio especificado.
+    */
     @PostMapping
     public ResponseEntity<HttpStatus> handleCreateInversion(@RequestBody final InversionDTO inversionDTO)
             throws IllegalStateException {
@@ -98,6 +128,12 @@ public class InversionesController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /**
+    Actualiza una inversion existente con la información proporcionada en un objeto InversionDTO.
+    @param inversionDTO El objeto InversionDTO que contiene la información de la inversion a actualizar.
+    @return Un objeto ResponseEntity con un código de estado HTTP que indica el resultado de la operación.
+    @throws EntityNotFoundException Si la inversion especificada no existe.
+    */
     @PutMapping
     public ResponseEntity<HttpStatus> handleUpdateInversion(@RequestBody final InversionDTO inversionDTO)
             throws EntityNotFoundException {
@@ -109,11 +145,14 @@ public class InversionesController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
-    // NOTA: Según logica de negocio, las inversiones no se deberían poder eliminar,
-    // solo liquidar
-    // Se agrega una transferencia hacia disponible
-    @PatchMapping("/liquidar/{idInversion}")
-    public ResponseEntity<HttpStatus> handleDeleteInversionById(@PathVariable final InversionDTO inversionDTO)
+    /**
+     * Liquida una inversion especificada por su ID
+     * @param inversionDTO El objeto InversionDTO que contiene el ID de la inversion a liquidar.
+     * @return Un objeto ResponseEntity con un código de estado HTTP que indica el resultado de la operación.
+     * @throws EntityNotFoundException Si la inversion especificada no existe.
+     */
+    @PatchMapping("/liquidar-inversion")
+    public ResponseEntity<HttpStatus> handleDeleteInversionById(@RequestBody final InversionDTO inversionDTO)
             throws EntityNotFoundException {
         if (!inversionesService.inversionAlreadyExist(inversionDTO.getId())) {
             throw new EntityNotFoundException("Inversion do not exist. UUID: " + inversionDTO.getId());
