02e4da7e296b6f05d5f8767cb7bbb5e30be02b39
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-04-15 16:57:25 -0500

    Movimientos controller comments added

--- src/main/java/com/cnunodevs/serverfinanceapp/controller/MovimientosController.java
+++ src/main/java/com/cnunodevs/serverfinanceapp/controller/MovimientosController.java
@@ -44,12 +44,29 @@ public class MovimientosController {
     private final MovimientosService movimientosService;
     private final MovimientoMapper movimientoMapper;
 
+    /**
+    Obtiene la métrica de balance de un usuario especificado por su ID. La métrica de balance incluye el saldo
+    actual del usuario y el total de ingresos y gastos registrados en su historial de movimientos.
+    @param idUsuario El ID del usuario para el cual se quiere obtener la métrica de balance.
+    @return Un objeto ResponseEntity con la métrica de balance del usuario y un código de estado HTTP que indica el
+    resultado de la operación.
+    */
     @GetMapping("/metrica/{idUsuario}")
     public ResponseEntity<MetricaBalance> handleGetMetricasBalance(@PathVariable final UUID idUsuario) {
         final MetricaBalance metrica = movimientosService.getMetricaBalanceByUsuario(idUsuario);
         return ResponseEntity.status(HttpStatus.OK).body(metrica);
     }
 
+
+    /**
+    Obtiene una página de los movimientos registrados para un usuario especificado por su ID. La página incluirá una
+    lista de objetos MovimientoDTO, que son los movimientos convertidos a un formato más legible y fácil de usar.
+    @param page El número de página que se quiere obtener (por defecto es 0).
+    @param size El tamaño de la página que se quiere obtener (por defecto es 9).
+    @param idUsuario El ID del usuario para el cual se quieren obtener los movimientos.
+    @return Un objeto ResponseEntity con la página de movimientos y un código de estado HTTP que indica el resultado
+    de la operación.
+    */
     @GetMapping("/{idUsuario}")
     public ResponseEntity<Page<MovimientoDTO>> handleGetMovimientosByUsuario(
             @RequestParam(defaultValue = "0") final Integer page,
@@ -64,6 +81,15 @@ public class MovimientosController {
         return ResponseEntity.status(HttpStatus.OK).body(pageMovimientosDTO);
     }
 
+    /**
+    Obtiene una página de los movimientos asociados a un presupuesto especificado por su ID. Se puede especificar el número
+    de página y la cantidad de resultados por página usando los parámetros "page" y "size".
+    @param page El número de página a obtener. El valor por defecto es 0.
+    @param size La cantidad de resultados por página. El valor por defecto es 9.
+    @param idPresupuesto El ID del presupuesto para el cual se quieren obtener los movimientos asociados.
+    @return Un objeto ResponseEntity con una página de los movimientos asociados al presupuesto especificado y un código
+    de estado HTTP que indica el resultado de la operación.
+    */
     @GetMapping("/{idPresupuesto}")
     public ResponseEntity<Page<MovimientoDTO>> handleGetMovimientosByPresupuesto(
             @RequestParam(defaultValue = "0") final Integer page,
@@ -78,6 +104,13 @@ public class MovimientosController {
         return ResponseEntity.status(HttpStatus.OK).body(pageMovimientosDTO);
     }
 
+    /**
+    Obtiene un movimiento por su ID.
+    @param idMovimiento El ID del movimiento que se quiere obtener.
+    @return Un objeto ResponseEntity con el movimiento solicitado en formato DTO y un código de estado HTTP que indica el
+    resultado de la operación.
+    @throws EntityNotFoundException Si el movimiento con el ID especificado no existe.
+    */
     @GetMapping("/{idMovimiento}")
     public ResponseEntity<MovimientoDTO> handleGetMovimientoById(@PathVariable final UUID idMovimiento) {
         if (!movimientosService.movimientoAlreadyExist(idMovimiento)) {
@@ -88,6 +121,14 @@ public class MovimientosController {
         return ResponseEntity.status(HttpStatus.OK).body(movimientoDTO);
     }
 
+    /**
+    Crea un nuevo movimiento asociado a un presupuesto. El movimiento debe ser no contabilizable y debe tener
+    especificado un ID de presupuesto. Si el movimiento es contabilizable o no tiene un ID de presupuesto, se
+    lanzará una excepción.
+    @param movimientoDTO El DTO del movimiento que se quiere crear.
+    @return Un objeto ResponseEntity con un código de estado HTTP que indica el resultado de la operación.
+    @throws IllegalStateException Si el movimiento es contabilizable o no tiene un ID de presupuesto.
+    */
     @PostMapping("/presupuesto")
     public ResponseEntity<HttpStatus> handleCreateMovimientoOfPresupuesto(
             @RequestBody final MovimientoDTO movimientoDTO)
@@ -101,6 +142,14 @@ public class MovimientosController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /**
+    Crea un nuevo movimiento, ya sea un ingreso o un gasto. Si se especifica "aplicaDescuentoEspecifico" como verdadero y el
+    movimiento es un ingreso, se aplica un descuento a una cuenta de ahorro específica (especificada por "idCuentaAhorroEspecifica").
+    @param movimientoDTO El objeto DTO que representa el movimiento a crear.
+    @param aplicaDescuentoEspecifico Booleano que indica si se aplica un descuento a una cuenta de ahorro específica.
+    @param idCuentaAhorroEspecifica El ID de la cuenta de ahorro específica a la que se le aplicará el descuento (si "aplicaDescuentoEspecifico" es verdadero).
+    @return Un objeto ResponseEntity con un código de estado HTTP que indica el resultado de la operación.
+    */
     @PostMapping
     public ResponseEntity<HttpStatus> handleCreateMovimiento(
             @RequestBody final MovimientoDTO movimientoDTO,
@@ -115,6 +164,13 @@ public class MovimientosController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /**
+    Elimina un movimiento existente en la base de datos.
+    @param movimientoDTO El objeto DTO del movimiento a eliminar.
+    @return Un objeto ResponseEntity con un código de estado HTTP que indica el resultado de la operación.
+    @throws EntityNotFoundException si no existe ningún movimiento con el ID especificado en el objeto DTO.
+    @throws IllegalCallerException si el movimiento es contabilizable y no se puede eliminar.
+    */
     @DeleteMapping
     public ResponseEntity<HttpStatus> handleDeleteMovimiento(@RequestBody final MovimientoDTO movimientoDTO)
             throws EntityNotFoundException, IllegalCallerException {
@@ -128,6 +184,12 @@ public class MovimientosController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    /**
+    Elimina una lista de movimientos existentes en la base de datos.
+    @param movimientosDTO Una lista de objetos DTO de los movimientos a eliminar.
+    @return Un objeto ResponseEntity con un código de estado HTTP que indica el resultado de la operación.
+    @throws IllegalCallerException si alguno de los movimientos es contabilizable y no se puede eliminar.
+    */
     @DeleteMapping
     public ResponseEntity<HttpStatus> handleDeleteListOfMovimientosByIds(
             @RequestBody final List<MovimientoDTO> movimientosDTO) throws IllegalCallerException {
