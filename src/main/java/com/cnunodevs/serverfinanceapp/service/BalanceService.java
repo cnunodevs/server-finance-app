8e70df858b14bf02570a2dee2e4b619b07a5779a
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-04-15 12:45:13 -0500

    Update BalanceService.java

--- src/main/java/com/cnunodevs/serverfinanceapp/service/BalanceService.java
+++ src/main/java/com/cnunodevs/serverfinanceapp/service/BalanceService.java
@@ -1,5 +1,14 @@
 package com.cnunodevs.serverfinanceapp.service;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import com.cnunodevs.serverfinanceapp.model.entity.Balance;
+
 public interface BalanceService {
     
+    Balance getBalanceByUsuario(UUID idUsuario);
+    void aumentarBalanceByUsuario(BigDecimal importe, UUID idUsuario);
+    void disminuirBalanceByUsuario(BigDecimal importe, UUID idUsuario);
+
 }
