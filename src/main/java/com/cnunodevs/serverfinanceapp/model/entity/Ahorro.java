3f996e74d377f12b6db29258fbf0f7da52a0d52c
cristianPorrety <cristianporretydev@gmail.com>
2023-04-16 13:48:09 -0500

    relationship between ahorro and objetivo was changed

--- src/main/java/com/cnunodevs/serverfinanceapp/model/entity/Ahorro.java
+++ src/main/java/com/cnunodevs/serverfinanceapp/model/entity/Ahorro.java
@@ -13,8 +13,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -43,12 +41,8 @@ public class Ahorro {
 
     private boolean automatico;
 
-    @ManyToMany
-    @JoinTable(
-        name = "ahorro_objetivo",
-        joinColumns = @JoinColumn(name = "ahorro_fk"),
-        inverseJoinColumns = @JoinColumn(name = "objetivo_fk")
-    )
+    @OneToOne
+    @JoinColumn(name = "objetivo_fk")
     private Objetivo objetivo;
 
     @OneToOne(mappedBy = "ahorro_fk")
