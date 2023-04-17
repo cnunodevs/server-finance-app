81efee947ed0cfde67e590fc9416dbb8229ce93b
cristianPorrety <cristianporretydev@gmail.com>
2023-04-17 16:12:51 -0500

    IA chatbot functionality added

---
+++ src/main/java/com/cnunodevs/serverfinanceapp/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.cnunodevs.serverfinanceapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.flashvayne.chatgpt.service.ChatgptService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/v1/chat")
+@RequiredArgsConstructor
+public class ChatController {
+    
+    private final ChatgptService chatgptService;
+
+    @GetMapping
+    public ResponseEntity<String> chatWith(@RequestParam String message) {
+        chatgptService.sendMessage("Simula ser el asistente financiero de una plataforma para el monitoreo de finanzas personales. Responderás mis dudas de manera de didáctica. Proveerás al menos un ejemplo del concepto o duda. Cuando realice una pregunta que no esté relacionada con temas financieros, responderás: Lo siento, solo estoy habilitado para responderte dudas sobre temáticas de tipo financiero. Después de este mensaje, responderás: Hola, soy tu asistente financiero virtual. ¿Tienes alguna duda?");
+        chatgptService.sendMessage("ademas, donde cada respuesta tenga maximo ");
+        return ResponseEntity.status(HttpStatus.OK).body(chatgptService.sendMessage(message));
+    }
+
+}
