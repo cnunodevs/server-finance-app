df4b8ae1bf1ee4daaf8d464b89cb88810c97c766
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-04-21 09:32:21 -0500

    Update Movimiento.java

--- src/main/java/com/cnunodevs/serverfinanceapp/model/entity/Movimiento.java
+++ src/main/java/com/cnunodevs/serverfinanceapp/model/entity/Movimiento.java
@@ -12,6 +12,8 @@ import com.cnunodevs.serverfinanceapp.model.entity.enums.TipoMovimiento;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -43,6 +45,7 @@ public class Movimiento {
     private BigDecimal importe;
 
     @Column(length = 50, nullable = false)
+    @Enumerated(EnumType.STRING)
     private TipoMovimiento tipo;
 
     @Column(length = 50, nullable = false)
